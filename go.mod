6526e62c9b1be4d0dbe8614114c7e485fa431192
gambtho <Thomas.Gamble@microsoft.com>
2021-07-07 14:33:58 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go_echo
+
+go 1.15
+
+require (
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/stretchr/testify v1.7.0
+)
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main
+import (
+"net/http"
+"net/http/httptest"
+"testing"
+
+"github.com/labstack/echo/v4"
+"github.com/stretchr/testify/assert"
+)
+
+func TestGetGhost(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/echo/statement")
+
+	// Assertions
+	if assert.NoError(t, getGhost(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "oooOOOoooOOOooo\n", rec.Body.String())
+	}
+
+}
+
+func TestGetEcho(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/echo/statement", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/echo/")
+	c.SetParamNames("statement")
+	c.SetParamValues("statement")
+
+	// Assertions
+	if assert.NoError(t, getEcho(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "statement statement\n", rec.Body.String())
+	}
+
+}
